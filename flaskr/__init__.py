185e47cb677dad94ef3267077c3a0c127336b1f8
red5885 <red5885@garretson.home>
2021-05-15 12:20:29 -0500

    generated database and module for accessing db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,13 @@
 import os
 
 from flask import Flask
+from . import db
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flasker.sqlite')
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
     )
 
     if test_config is None:
@@ -22,5 +23,7 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    db.init_app(app)
 
     return app
