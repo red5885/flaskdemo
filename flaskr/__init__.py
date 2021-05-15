3121af8310bf63cd79743d23b7b91c629597b154
red5885 <red5885@garretson.home>
2021-05-15 17:52:36 -0500

    Created blog and installer. debugged

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from . import db, auth
+from . import db, auth, blog
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -27,5 +27,7 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
