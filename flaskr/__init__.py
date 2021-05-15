2f0dc25a2cae10ccbd100126e5e4f905ba40b239
red5885 <red5885@garretson.home>
2021-05-15 12:42:18 -0500

    created authentication backend. Need to create register and login templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from . import db
+from . import db, auth
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -25,5 +25,7 @@ def create_app(test_config=None):
         return 'Hello, World!'
 
     db.init_app(app)
+
+    app.register_blueprint(auth.bp)
 
     return app
